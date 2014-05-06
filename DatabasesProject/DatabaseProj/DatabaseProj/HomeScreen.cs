6f538b9dd1fabcd6fe32312b406d9585edad49e5
Nick Ganter <nickganter91@gmail.com>
2014-05-05 20:16:47 -0500

    Home Screen
    DataGridView displaying search results.
    Event handlers added, going to start the edit student dialog.

--- DatabasesProject/DatabaseProj/DatabaseProj/HomeScreen.cs
+++ DatabasesProject/DatabaseProj/DatabaseProj/HomeScreen.cs
@@ -15,12 +15,13 @@ namespace DatabaseProj
     public partial class HomeScreen : Form
     {
 		public DbprojDatabase Database { get; set; }
-
+        private DataGridView dg;
         public HomeScreen()
         {
             InitializeComponent();
             Size = new Size((int)(Screen.PrimaryScreen.Bounds.Width / 1.5), (int)(Screen.PrimaryScreen.Bounds.Height / 1.5));
 			Database = new DbprojDatabase();
+            cbEmphasis.SelectedIndex = 0;
             CenterToScreen();
         }
 
@@ -41,5 +42,40 @@ namespace DatabaseProj
 				MessageBox.Show( "Adding failure.", "Error" );
 			}
 		}
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtFirstName.Clear();
+            txtLastName.Clear();
+            cbEmphasis.SelectedIndex = 0;
+            dg.Rows.Clear();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            List<Student> students = Database.GetStudentLikeFirstAndLastNameAndEmphasis(txtFirstName.Text, txtLastName.Text, cbEmphasis.SelectedText);
+            dg = dgStudentSearchResults;
+            foreach (Student s in students)
+            {
+                dg.Rows.Add(s.UniversityId, s.FirstName, s.MiddleName, s.LastName, s.Status, s.Emphasis.Id, s.Emphasis.Name, new Button { Text = "Edit" }.Text = "Edit");
+                dg.CellClick += new DataGridViewCellEventHandler(dg_EventHandler);
+            }
+            
+        }
+
+        private void dg_EventHandler(object args, DataGridViewCellEventArgs e)
+        {
+            int colIndex = e.ColumnIndex;
+            int rowIndex = e.RowIndex;
+
+            DataGridViewRow row = dg.Rows[rowIndex];
+            //will do stuff wooohoooo
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+            Application.Exit();
+        }
     }
 }
